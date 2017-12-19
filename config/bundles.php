2402c340e46736fe3c8a3df98704f00b3cdc7c8d
Prohorovych Andrew <prohorovychua@gmail.com>
2017-12-19 11:56:16 +0200

    install NelmioCorsBundle

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
