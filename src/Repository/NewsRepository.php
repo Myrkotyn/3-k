12aff0d056174d7f96be21cbdc698ca659a0a6e0
Prohorovych Andrew <prohorovychua@gmail.com>
2017-12-22 02:16:45 +0200

    refactoring

--- src/Repository/NewsRepository.php
+++ src/Repository/NewsRepository.php
@@ -8,21 +8,4 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
 
 class NewsRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
-    {
-        parent::__construct($registry, News::class);
-    }
-
-    /*
-    public function findBySomething($value)
-    {
-        return $this->createQueryBuilder('n')
-            ->where('n.something = :value')->setParameter('value', $value)
-            ->orderBy('n.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
 }
